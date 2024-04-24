0e9a99d5742cfea813f1124995b9b53dcf8e0af0
Donchess1 <stepabod@.com>
2024-04-24 12:35:55 +0000

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""main class"""
+def pascal_triangle(n):
+    """class triangle"""
+    
+    triangle=[]
+    for x in range (n):
+        row = []
+        row.append(1)
+        for y in range (1,x):
+            row.append(triangle[x-1][y-1] + triangle[x-1][y])
+        if x > 0:
+            row.append(1)
+        triangle.append(row)
+    return triangle
