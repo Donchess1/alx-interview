8be5f5472e9fc83ea452edb9adb26c4a9951e21b
Donchess1 <Stepabod@yahoo.com>
2024-04-26 09:52:06 +0100

    Upd-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,7 +9,7 @@ def pascal_triangle(n):
     for x in range(n):
         row = []
         row.append(1)
-        for y in range(1 , x):
+        for y in range(1, x):
             row.append(triangle[x-1][y-1] + triangle[x-1][y])
         if x > 0:
             row.append(1)
