aa04e2720331c0f9dbd398bd438ce66082c18805
Donchess1 <stepabod@.com>
2024-05-02 20:11:43 +0000

    lock

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''A module for working with lockboxes.
+'''
+
+
+def canUnlockAll(boxes):
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked    '''
+    n = len(boxes)
+    visited = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in visited:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            visited.add(boxIdx)
+    return n == len(seen_boxes)
