3327ea3daeec6dff701762de0f6e9c9945c52e92
Donchess1 <stepabod@.com>
2024-06-13 19:53:40 +0000

    exe 0-rot

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+"""
+THis rotates the 2d matrix by first transposing then swapping
+positions
+"""
 def rotate_2d_matrix(matrix):
-    """
-    THis rotates the 2d matrix by first transposing then swapping
-    positions
 
-    """
     for a in range(len(matrix)):
         for b in range(a, len(matrix)):
             matrix[a][b], matrix[b][a] = matrix[b][a], matrix[a][b]
     for a in range(len(matrix)):
         matrix[a].reverse()
+    return matrix
