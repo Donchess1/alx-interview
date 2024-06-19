fd5b0bab81078f72a28b2bebcec4c750c200895d
Donchess1 <stepabod@.com>
2024-06-19 13:15:15 +0000

    retry

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,10 +2,11 @@
 
 """coin change using greedy algorithm"""
 
+
 def makeChange(coins, total):
-    trial = [float ('inf')] * (total+1) #creating default counter list 0-total+1
-    trial[0]= 0 # zeroing the first element
+    trial = [float('inf')] * (total+1)   # creating default counter list
+    trial[0] = 0   # zeroing the first element
     for coin in coins:
-        for x in range (coin, total+1):
-            trial[x] = min(trial[x], trial[x-coin]+1)
-    return trial[total] if trial[total] != float ('inf') else -1
+        for x in range(coin, total + 1):
+            trial[x] = min(trial[x], trial[x-coin] + 1)
+    return trial[total] if trial[total] != float('inf') else -1
